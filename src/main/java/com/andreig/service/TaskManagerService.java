80485ba33d32d3a135ed851dbcc1e8b6e230acdc
Stan Clarke <1991.andrei@gmail.com>
2021-08-08 12:29:34 +0300

    Added Maven-Jar-Plugin and MANIFEST.MF

--- src/main/java/com/andreig/service/TaskManagerService.java
+++ src/main/java/com/andreig/service/TaskManagerService.java
@@ -2,7 +2,6 @@ package com.andreig.service;
 
 public class TaskManagerService {
     public static void main(String[] args) {
-        System.out.println(args[0]);
-        System.out.println(args[1]);
+
     }
 }
